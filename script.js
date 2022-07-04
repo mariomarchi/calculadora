f32696a28cf95c319c5319915082eb070ca4a2c7
mariomarchi <malbertomarchi@gmail.com>
2022-07-04 15:47:27 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function insertNum(num) {
+    var numero = document.querySelector("#screen").innerHTML;
+    document.querySelector("#screen").innerHTML = numero + num;
+}
+
+function clearResult()
+{
+    document.querySelector("#screen").innerHTML = "";
+}
+
+function backSpace()
+{
+    var resultado = document.querySelector("#screen").innerHTML
+    document.querySelector("#screen").innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcularOperacao()
+{
+    var resultado = document.querySelector("#screen").innerHTML;
+    if(resultado)
+    {
+        document.querySelector("#screen").innerHTML = eval(resultado);
+        return
+    }
+    else(resultado)
+    {
+    var resultado = document.querySelector("#screen").innerHTML = "";
+    alert("Nada para calcular, digite sua operação matemática")
+    }
+
+}
